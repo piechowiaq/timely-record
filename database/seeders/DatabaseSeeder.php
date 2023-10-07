ff791ce619ee92848a87d04d9e89f864257692c7
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-10-07 18:42:05 +0200

    Getting ready for deployment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Company;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
@@ -17,9 +19,22 @@ class DatabaseSeeder extends Seeder
     {
         Role::create(['name' => 'Super Admin']);
 
-        User::factory()->create([
-             'name' => 'Test User',
+        $company = Company::create([
+            'name' => 'Hotel',
+            'city' => 'Warszawa',
+        ]);
+
+        $user = User::factory()->create([
+             'name' => 'Super Admin',
              'email' => 'contact@timelyrecord.com',
+             'password' => Hash::make('12345678')
+
          ])->assignRole('Super Admin');
+
+        $company->users()->attach($user);
+
+        $this->call([
+            RegistrySeeder::class,
+        ]);
     }
 }
--- resources/js/Layouts/WorkspaceLeftNavigationBar.vue
+++ resources/js/Layouts/WorkspaceLeftNavigationBar.vue
@@ -36,10 +36,24 @@ defineProps({
                 <div class="text-zinc-300 group-hover:text-white">Registers</div>
             </Link>
         </div>
-      <hr>
 
-        <div v-if="companiesCount > 1" class="my-4">
-            <Link :href="route('workspace.selector')" class="flex items-center group py-3">
+
+        <div v-if="$page.props.auth.user.id === 1">
+            <hr class="p-4">
+
+            <Link :href="route('admin.dashboard')" class="flex items-center group">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                     class="w-4 h-4 mr-2 fill-zinc-400 group-hover:fill-white">
+                    <path
+                        d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm-5.6-4.29a9.95 9.95 0 0 1 11.2 0 8 8 0 1 0-11.2 0zm6.12-7.64l3.02-3.02 1.41 1.41-3.02 3.02a2 2 0 1 1-1.41-1.41z"></path>
+                </svg>
+                <div class="text-zinc-300 group-hover:text-white">Admin</div>
+            </Link>
+        </div>
+
+        <div v-else-if="companiesCount > 1">
+            <hr class="p-4">
+            <Link :href="route('workspace.selector')" class="flex items-center group">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2 fill-zinc-400 group-hover:fill-white" viewBox="0 0 20 20">
                     <path d="M22 21H2v-2h1V4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v5h2v10h1v2Zm-5-2h2v-8h-6v8h2v-6h2v6Zm0-10V5H5v14h6V9h6ZM7 11h2v2H7v-2Zm0 4h2v2H7v-2Zm0-8h2v2H7V7Z"/>
                 </svg>
