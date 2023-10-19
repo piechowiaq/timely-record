1f2e4e7185d9350363273cf52115d447ad18dda6
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-10-19 21:19:06 +0200

    Cards for Dashboard

--- app/Http/Controllers/Workspace/WorkspaceDashboardController.php
+++ app/Http/Controllers/Workspace/WorkspaceDashboardController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Workspace;
 
 use App\Http\Controllers\Controller;
 use App\Models\Company;
+use App\Models\Registry;
+use App\Models\Report;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -14,6 +16,46 @@ class WorkspaceDashboardController extends Controller
 {
     public function __invoke(Company $company): Response
     {
+
+
+
+        $registries = DB::table('registries')
+            ->join('company_registry', 'registries.id', '=', 'company_registry.registry_id')
+            ->leftJoin(DB::raw('(
+        SELECT registry_id, company_id, MAX(expiry_date) as max_expiry_date
+        FROM reports
+        GROUP BY registry_id, company_id
+    ) as max_reports'), function ($join) {
+                $join->on('company_registry.registry_id', '=', 'max_reports.registry_id')
+                    ->on('company_registry.company_id', '=', 'max_reports.company_id');
+            })
+            ->where('company_registry.company_id', '=', $company->id)
+            ->where('assigned', true)
+            ->whereNotNull('max_reports.max_expiry_date')
+            ->where('max_reports.max_expiry_date', '<', Carbon::now()) // Only registries that have expired
+            ->select(
+                'company_registry.registry_id',
+                'registries.name',
+                'max_reports.max_expiry_date as expiry_date',
+                'company_registry.company_id'
+            )
+            ->groupBy(
+                'company_registry.registry_id',
+                'registries.name',
+                'company_registry.company_id',
+                'max_reports.max_expiry_date'
+            )
+            ->orderBy('max_reports.max_expiry_date', 'asc') // Ordering by oldest expiry dates
+            ->limit(3);
+
+
+        $mostOutdatedRegistries = $registries->get()->map(function ($registry) {
+            return [
+                'name' => $registry->name,
+                'expiry_date' => $registry->expiry_date
+            ];
+        })->all();
+
         $query = DB::table('registries')
             ->join('company_registry', 'registries.id', '=', 'company_registry.registry_id')
             ->leftJoin(DB::raw('(
@@ -26,6 +68,8 @@ class WorkspaceDashboardController extends Controller
             })
             ->where('company_registry.company_id', '=', $company->id)
             ->where('assigned', true)
+            ->whereNotNull('max_reports.max_expiry_date')
+            ->where('max_reports.max_expiry_date', '>', Carbon::now()) // Only registries that haven't expired yet
             ->select(
                 'company_registry.registry_id',
                 'registries.name',
@@ -37,7 +81,25 @@ class WorkspaceDashboardController extends Controller
                 'registries.name',
                 'company_registry.company_id',
                 'max_reports.max_expiry_date'
-            );
+            )
+            ->orderBy('max_reports.max_expiry_date', 'desc') // Ordering by the most recent expiry dates
+            ->limit(3);
+
+
+
+// Pluck the names and expiry dates
+        $recentlyUpdatedRegistries = $query->get()->map(function ($registry) {
+            return [
+                'name' => $registry->name,
+                'expiry_date' => $registry->expiry_date
+            ];
+        })->all();
+
+
+
+
+
+
 
 
         $companiesCount = Auth::user()->companies()->count();
@@ -45,9 +107,9 @@ class WorkspaceDashboardController extends Controller
         return Inertia::render('Workspace/Dashboard', [
             'company' => $company,
             'companiesCount' => $companiesCount,
-            'registries' => $query->paginate(10)
-                ->withQueryString(),
-            'countOfUpToDateRegistries' => $query->paginate(10)
+            'mostOutdatedRegistries' => $mostOutdatedRegistries,
+            'recentlyUpdatedRegistries' => $recentlyUpdatedRegistries,
+            'countOfUpToDateRegistries' => $registries->paginate(10)
                 ->withQueryString()->whereNotNull('expiry_date')->where('expiry_date', '>', Carbon::now() )->count()
 
         ]);
--- resources/js/Pages/Workspace/Dashboard.vue
+++ resources/js/Pages/Workspace/Dashboard.vue
@@ -12,8 +12,11 @@ const props = defineProps({
     companiesCount: {
         type: Number,
     },
-    registries: {
-        type: Object,
+    mostOutdatedRegistries: {
+        type: Array,
+    },
+    recentlyUpdatedRegistries: {
+        type: Array,
     },
     countOfUpToDateRegistries: {
         type: Number
@@ -21,26 +24,26 @@ const props = defineProps({
 });
 
 
-const chartDataRegistries = ref({
-    labels: ['Aktualne', 'Nieaktualne'],
-    datasets: [
-        {
-            label: 'Registries',
-            backgroundColor: ['#9ae6b4', '#FEB2B2',],
-            data: [props.countOfUpToDateRegistries, props.registries.total - props.countOfUpToDateRegistries ],
-            borderWidth: 0
+// const chartDataRegistries = ref({
+//     labels: ['Aktualne', 'Nieaktualne'],
+//     datasets: [
+//         {
+//             label: 'Registries',
+//             backgroundColor: ['#9ae6b4', '#FEB2B2',],
+//             data: [props.countOfUpToDateRegistries, props.registries.total - props.countOfUpToDateRegistries],
+//             borderWidth: 0
+//
+//         }
+//     ],
+// });
 
-        }
-    ],
-});
+// const chartOptions = ref({
+//     cutout: 75,
+// });
 
-const chartOptions =ref({
-        cutout: 75,
-});
+const width = ref(120)
 
-const width =  ref(120)
-
-const height =  ref(100)
+const height = ref(100)
 
 
 </script>
@@ -48,53 +51,91 @@ const height =  ref(100)
 <template>
 
 
-<WorkspaceLayout :company="company" :companies-count="companiesCount">
-    <div class="bg-gray-100 py-2 px-3 h-12 items-center flex font-bold text-cyan-600">
-        <p>Dashboard</p>
-    </div>
-    <div class="md:flex space-x-2">
-        <div class="md:w-1/4">
-            <div class="border border-cyan-600 py-2 px-3 font-bold text-gray-600 mt-2">
-                <div class="justify-between items-center flex border-b">
-                    <p class="text-sm  pb-2">Registries</p>
-                    <p class="bg-green-500 px-2 mb-2 rounded text-white text-xs font-medium">VERY GOOD</p>
-                </div>
+    <WorkspaceLayout :company="company" :companies-count="companiesCount">
+        <div class="bg-gray-100 py-2 px-3 h-12 items-center flex font-bold text-cyan-600">
+            <p>Dashboard</p>
+        </div>
+        <div class="flex space-x-2">
+            <div class="md:w-1/2">
+                <section class="border h-full border-cyan-600 p-8 rounded-lg shadow-2xl font-bold text-gray-600 mt-2">
+                    <header class="justify-between items-center flex border-b pb-2">
+                        <p class="text-sm">Registries</p>
+                        <span class="bg-green-500 px-2 mb-2 rounded text-white text-xs font-medium">VERY GOOD</span>
+                    </header>
 
-                <div class="flex justify-between">
-                    <p class="text-xl py-2">85%</p>
-                    <div class="py-2">
-                        <p class="text-xs ">8 <span class="text-green-600">valid</span></p>
-                        <p class="text-xs ">18 <span class="text-red-600">expired</span></p>
+                    <div class="flex justify-between py-2">
+                        <p class="text-2xl">85%</p>
+                        <div>
+                            <p class="text-xs"><span class="text-green-600">8 valid</span></p>
+                            <p class="text-xs"><span class="text-red-600">18 expired</span></p>
+                        </div>
+                    </div>
+
+                    <article class="text-xs py-2">
+                        <h1 class="py-4">Most Outdated:</h1>
+                        <ul class="text-cyan-600">
+                            <li v-for="registry in mostOutdatedRegistries" :key="registry.name" class="py-1">
+                                {{ registry.name }} - {{ registry.expiry_date }}
+                            </li>
+                        </ul>
+                    </article>
+
+                    <article class="text-xs py-2">
+                        <h1 class="py-4">Recently updated:</h1>
+                        <ul class="text-cyan-600">
+                            <li v-for="registry in recentlyUpdatedRegistries" :key="registry.name" class="py-1">
+                                {{ registry.name }} - {{ registry.expiry_date }}
+                            </li>
+                        </ul>
+                    </article>
+
+                </section>
+            </div>
+
+
+
+
+            <div class="md:w-1/2 ">
+                <div class="h-full border bg-gray-100 border-gray-300 p-8 rounded-lg font-bold text-gray-300 mt-2">
+                    <div class="justify-between items-center flex border-b">
+                        <p class="text-sm  pb-2">Trainings</p>
+                        <p class="bg-red-200 px-2 mb-2 rounded text-white text-xs font-medium">NEEDS IMPROVEMENT</p>
+                    </div>
+
+                    <div class="flex justify-between">
+                        <p class="text-2xl py-2">65%</p>
+                        <div class="py-2">
+                            <p class="text-xs ">8 <span class="text-green-200">valid</span></p>
+                            <p class="text-xs ">18 <span class="text-red-200">expired</span></p>
+
+                        </div>
+
 
                     </div>
 
-
+                    <div class="text-xs py-2">
+                        <h1 class="py-4">Most Outdated:</h1>
+                        <ul class="text-gray-200">
+                            <li class="py-1">Kontrola okresowa stanu technicznego obiektu</li>
+                            <li class="py-1">Kontrola okresowa stanu technicznego obiektu</li>
+                            <li class="py-1">Kontrola okresowa stanu technicznego obiektu</li>
+                        </ul>
+                    </div>
+                    <div class="text-xs py-2">
+                        <h1 class="py-4">Recently updated:</h1>
+                        <ul class="text-gray-200">
+                            <li class="py-1">Kontrola okresowa stanu technicznego obiektu</li>
+                            <li class="py-1">Kontrola okresowa stanu technicznego obiektu</li>
+                            <li class="py-1">Kontrola okresowa stanu technicznego obiektu</li>
+                        </ul>
+                    </div>
                 </div>
 
-                <div class="text-xs py-2">
-                    <h1>Most Outdated:</h1>
-                    <ul class="text-cyan-600">
-                        <li>Kontrola okresowa stanu technicznego obiektu</li>
-                        <li>Kontrola okresowa stanu technicznego obiektu</li>
-                        <li>Kontrola okresowa stanu technicznego obiektu</li>
-                    </ul>
-                </div>
-                <div class="text-xs py-2">
-                    <h1>Recently updated:</h1>
-                    <ul class="text-cyan-600">
-                        <li>Kontrola okresowa stanu technicznego obiektu</li>
-                        <li>Kontrola okresowa stanu technicznego obiektu</li>
-                        <li>Kontrola okresowa stanu technicznego obiektu</li>
-                    </ul>
-                </div>
             </div>
-<!--            <DoughnutChart :chart-data="chartDataRegistries" class="w-full p-8"/>-->
+
         </div>
 
-    </div>
 
-
-
-</WorkspaceLayout>
+    </WorkspaceLayout>
 
 </template>
