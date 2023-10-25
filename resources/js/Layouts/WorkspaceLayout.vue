241aa473573786931181ce0f76179d4c0bab4865
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-10-25 16:00:57 +0200

    Refactoring profile'

--- resources/js/Layouts/WorkspaceLayout.vue
+++ resources/js/Layouts/WorkspaceLayout.vue
@@ -77,7 +77,7 @@ const hasMultipleCompanies = WorkspaceMenu.hasMultipleCompanies;
                                                     type="button"
                                                     class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
                                                 >
-                                                    {{ $page.props.auth.user.name }}
+                                                     {{ $page.props.auth.user.first_name }}  {{ $page.props.auth.user.last_name }}
 
                                                     <svg
                                                         class="ml-2 -mr-0.5 h-4 w-4"
@@ -180,13 +180,13 @@ const hasMultipleCompanies = WorkspaceMenu.hasMultipleCompanies;
                     <div class="pt-4 pb-1 border-t border-gray-200">
                         <div class="px-4">
                             <div class="font-medium text-base text-gray-800">
-                                {{ $page.props.auth.user.name }}
+                                {{ $page.props.auth.user.first_name }}  {{ $page.props.auth.user.last_name }}
                             </div>
                             <div class="font-medium text-sm text-gray-500">{{ $page.props.auth.user.email }}</div>
                         </div>
 
                         <div class="mt-3 space-y-1">
-                            <ResponsiveNavLink v-if="!isSuperAdmin" :href="route('profile.edit')"> Profile</ResponsiveNavLink>
+<!--                            <ResponsiveNavLink v-if="!isSuperAdmin" :href="route('profile.edit')"> Profile</ResponsiveNavLink>-->
                             <ResponsiveNavLink :href="route('logout')" method="post" as="button">
                                 Log Out
                             </ResponsiveNavLink>
--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -17,7 +17,8 @@ defineProps({
 const user = usePage().props.auth.user;
 
 const form = useForm({
-    name: user.name,
+    first_name: user.first_name,
+    last_name: user.last_name,
     email: user.email,
 });
 </script>
@@ -32,15 +33,25 @@ const form = useForm({
             </p>
         </header>
 
-        <form @submit.prevent="form.patch(route('profile.update'))" class="mt-6 space-y-6">
+        <form @submit.prevent="form.patch(route('users.update', user))" class="mt-6 space-y-6">
             <div>
-                <InputLabel for="name" value="Name" />
+                <InputLabel for="firstName" value="First Name" />
 
                 <TextInput
-                    id="name"
+                    id="firstName"
                     type="text"
                     class="mt-1 block w-full"
-                    v-model="form.name"
+                    v-model="form.first_name"
+                    required
+                    autofocus
+                    autocomplete="name"
+                />
+                <InputLabel for="lastName" value="Last Name" />
+                <TextInput
+                    id="lastName"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.last_name"
                     required
                     autofocus
                     autocomplete="name"
